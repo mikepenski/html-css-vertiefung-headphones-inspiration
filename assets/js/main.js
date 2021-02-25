7d5e0e2a2e1c242739396b5c1de62700bb791eb2
Mike <penskimike@gmail.com>
2021-02-25 10:17:25 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+
+    const body = document.querySelector("body");
+    body.classList.remove("preload");
+
+
+    const mainNav = document.querySelector("#main-navigation");
+
+    //setTimeout(function(){
+        mainNav.classList.add("slide-in");
+    //}, 500);
+
+}
